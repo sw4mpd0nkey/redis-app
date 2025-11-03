0ebb8f234419bd8b8a5731294fa122e18dd83019
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-11-02 20:28:22 -0500

    some more post merge clean up + refactoring

--- datatypes/vector/vector.h
+++ datatypes/vector/vector.h
@@ -25,5 +25,20 @@ typedef struct vector vector;
 // create new vector
 vector *create_vector(int word_size, conn_map* elements);
 
+unsigned int get_size(vector *vector);
+
+unsigned int get_capacity(vector *vector);
+
+int is_empty(vector *vector);
+
+void push_back(vector *vector, void *data);
+
+void pop_back(vector *vec);
+
+void delete_vector(vector *vector);
+
+void *get(vector *vector, unsigned int index);
+
+void *set(vector *vector, void *data, unsigned int index);
 
 #endif // VECTOR_H
