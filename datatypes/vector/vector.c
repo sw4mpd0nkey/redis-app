923913a9c486ac362dd7378e590609e806cf268a
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-11-02 20:28:01 -0500

    some more post merge clean up + refactoring

--- datatypes/vector/vector.c
+++ datatypes/vector/vector.c
@@ -5,13 +5,12 @@
 static const int DEFAULT_CAPACITY = 4;
 
 
-vector *create_vector(int word_size, int (*compare)(const void *, const void *)) {
+vector *create_vector(int word_size, conn_map* elements) {
     vector *newboi = (vector *)calloc(1, sizeof(vector));
-    newboi->cmp = compare;
     newboi->size = 0;
     newboi->capacity = DEFAULT_CAPACITY;
     newboi->word_size = word_size;
-    newboi->elements = calloc(DEFAULT_CAPACITY, word_size);
+    newboi->elements = &elements;
 
     return newboi;
 }
@@ -37,12 +36,12 @@ int is_empty(vector *vector) {
 void push_back(vector *vector, void *data) {
 
     // verify that there is space in the vector
-    if (size(vector) < capacity(vector)) {
+    if (get_size(vector) < get_capacity(vector)) {
 
         // no need to resize so set a pointer to the elements attribute
         char *ptr = (char *)vector->elements;
         // move the pointer to the end of the vector 
-        ptr += (size(vector) * (vector->word_size));
+        ptr += (get_size(vector) * (vector->word_size));
         // create a copy of the data and tack that onto the vector
         memcpy((void *)ptr, (const void *)data, vector->word_size);
         // increase the size value
@@ -51,11 +50,11 @@ void push_back(vector *vector, void *data) {
         // needs to be resized so we create a tmp pointer to the current data
         void *tmp = vector->elements;
         // set the vectors elements to a new bigger size
-        vector->elements = calloc(2 * capacity(vector), vector->word_size);
+        vector->elements = calloc(2 * get_capacity(vector), vector->word_size);
         // increase capacity size
-        vector->capacity = 2 * capacity(vector);
+        vector->capacity = 2 * get_capacity(vector);
         // add new tmp data section to the vectors elements
-        memcpy(vector->elements, tmp, size(vector) * vector->word_size);
+        memcpy(vector->elements, tmp, get_size(vector) * vector->word_size);
         // clear the data at tmp
         free(tmp);
         // call push_back again with the same data and it'll be added to the vector
@@ -64,7 +63,7 @@ void push_back(vector *vector, void *data) {
 }
 
 void pop_back(vector *vec) {
-    if (size(vec) != 0) {
+    if (get_size(vec) != 0) {
         vec->size -= 1;
     }
 }
@@ -81,7 +80,7 @@ void *get(vector *vector, unsigned int index) {
 }
 
 void *set(vector *vector, void *data, unsigned int index) {
-    if (index < capacity(vector)) {
+    if (index < get_capacity(vector)) {
         void *ptr = (void *)((char *)vector->elements + (index * (vector->word_size)));
         memcpy(ptr, (const void *)data, vector->word_size);
     
@@ -93,61 +92,4 @@ void *set(vector *vector, void *data, unsigned int index) {
     }
   
     return NULL;
-}
-
-void shrinkToFit(vector *vector) {
-  if (size(vector) != 0) {
-    void *tmp = vector->elements;
-    vector->elements = calloc(vector->size, vector->word_size);
-    memcpy(vector->elements, tmp, size(vector) * vector->word_size);
-    free(tmp);
-    vector->capacity = vector->size;
-  }
-}
-
-void insert(vector *vector, void *elemAddr, unsigned int position) {
-
-  unsigned int len = size(vector);
-  
-  if (len == position) {
-    push_back(vector, elemAddr);
-    return;
-  
-} else {
-
-    push_back(vector, (char *)vector->elements + (len - 1) * vector->word_size);
-    memmove((void *)((char *)vector->elements + (position + 1) * vector->word_size),
-            (void *)((char *)vector->elements + position * vector->word_size),
-            (len - position - 1) * vector->word_size);
-    set(vector, elemAddr, position);
-    return;
-  }
-}
-
-void removeAt(vector *vector, unsigned int position) {
-
-  int len = size(vector);
-  if (position == len - 1) {
-    pop_back(vector);
-  } else {
-    for (unsigned int i = position + 1; i < size(vector); i++) {
-      set(vector, get(vector, i), i - 1);
-    }
-    (vector->size)--;
-  }
-}
-
-void printAll(vector *vector, void (*printFunc)(const void *),
-              char printNewLineAtTheEnd) {
-  for (unsigned int i = 0; i < size(vector); i++) {
-    printFunc(get(vector, i));
-  }
-  if (printNewLineAtTheEnd) {
-    printf("\n");
-  }
-}
-
-// uses the built in qsort to sort the generic vector
-void sort(vector *vector) { 
-    qsort(vector->elements, size(vector), vector->word_size, vector->cmp); 
 }
--- datatypes/vector/vector.h
+++ datatypes/vector/vector.h
@@ -4,46 +4,26 @@
 #pragma once
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h> typedef struct {
+#include <string.h> 
+#include "../connection/conn.h"
+
+typedef struct {
 	conn_t* connection;
  } conn_map;
 
 // vector type
-typedef struct
+struct vector
 {
-	vector_size_t size;
-	vector_type_t capacity;
+	unsigned int size;
+	unsigned int capacity;
+	unsigned int word_size;
 	conn_map* elements[];
-} vector_data_t;
+};
 
-// returns the meta data for the vector
-vector_data_t* vector_get_data(vector vec);
-// creates new vector
-vector_data_t* vector_new();
-// create a new vector instance
-vector vector_create(void);
-// destroy specified vector
-void vector_destroy(vector vector);
-// returns current size of the specified vector
-vector_size_t vector_size(vector vector);
-// returns current capacity of specified vector
-vector_size_t vector_capacity(vector vector);
-// add element to the vector
-void vector_push_back(vector* vector_address, vector_type_t type_size);
+typedef struct vector vector;
 
-///////////////////////////////////////////////////////////
-
-
-// access element at index in vector obj
-int vector_at(vector_data_t* obj, const int index);
-// access beginning of vector
-int* vector_begin(vector_data_t* obj);
-// access end of vector
-int* vector_end(vector_data_t* obj);
-// pops last element off vector
-void vector_pop_back(vector_data_t* obj);
-// erase element
-void vector_erase(vector_data_t* obj, int* first, int* last); 
+// create new vector
+vector *create_vector(int word_size, conn_map* elements);
 
 
 #endif // VECTOR_H
